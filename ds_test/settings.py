648c10deb7da1b55f7e4a379c193550d5d955627
mahfujul.hasan <mahfujul.hasan@bjitgroup.com>
2023-10-05 15:28:15 +0600

    test case passed for page app home view

--- ds_test/settings.py
+++ ds_test/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     
     'api',
     'accounts',
+    'pages',
 ]
 AUTH_USER_MODEL = "accounts.CustomUser"
 
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'ds_test.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
